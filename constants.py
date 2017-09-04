ad813ec1f59433533b93ef5364b14fb84f1d9428
Marc Górriz Blanch <gbmarc@hotmail.es>
2017-09-04 17:26:40 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,32 +1,45 @@
 # PATH definition
-initial_weights_path = "models/unet_augment10epoch.hdf5"
-final_weights_path = "models/unet_augment10epoch_test1.hdf5"
+global_path = "classical/"
+#global_path = "init_1000"
+initial_weights_path = "models/classical.hdf5"
+#initial_weights_path = "test_init/initmodel9.h5"
+#initial_weights_path = "models/init_1000.hdf5"
+#initial_weights_path = "css_15_0_0/models/model4.h5"
+final_weights_path = "models/classicalo.hdf5"
 
 # Data definition
 img_rows = 64 * 3
 img_cols = 80 * 3
 
 nb_classes = 10
-nb_train = 2000
+nb_total = 2000
+nb_train = 1600
 
-nb_labeled = 600
+#nb_labeled = 600
+nb_labeled = nb_train
 nb_unlabeled = nb_train - nb_labeled
 
 # CEAL parameters
-uncertain_method = "variance"
-augmentation = True
 apply_edt = True
-nb_iterations = 3
-nb_annotations = 10
-nb_pseudo = 100
-nb_step_predictions = 10
+nb_iterations = 10
 
-initial_train = False
-nb_initial_epochs = 5
-nb_active_epochs = 1
+nb11 = 10
+nb12 = 5
+nb2 = 10
+nb31 = 10
+nb32 = 15
+
+rate = 10
+nb_step_predictions = 20
+pseudo_epoch = 5
+first_epoch = 5
+second_epoch = 0
+thirt_epoch = 0
+nb_pseudo_initial = 20
+pseudo_rate = 20
+
+initial_train = True
+apply_augmentation = False
+nb_initial_epochs = 10
+nb_active_epochs = 2
 batch_size = 128
-
-#logs
-path_log = "only_oracle_aaug/"
-sel_random = False
-nb_log_pseudo = 10
