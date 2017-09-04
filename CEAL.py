d07407d90b462522ef25ab17aad3cb4a548c5213
Marc Górriz Blanch <gbmarc@hotmail.es>
2017-09-04 17:23:23 +0200

    Update CEAL.py

--- CEAL.py
+++ CEAL.py
@@ -1,97 +1,66 @@
 from __future__ import print_function
 
 from keras.callbacks import ModelCheckpoint
-from keras.preprocessing.image import ImageDataGenerator
 
 from data import load_train_data
 from unet import *
 from utils import *
 from constants import *
 
+create_paths()
+log_file = open(global_path+"logs/log_file.txt",'a')
+
+# CEAL data definition
 X_train, y_train = load_train_data()
-X_train = preprocess(X_train)
-y_train = preprocess(y_train)
-
-X_train = X_train.astype('float32')
-mean = np.mean(X_train)  # mean for data centering
-std = np.std(X_train)  # std for data normalization
-
-X_train -= mean
-X_train /= std
-
-y_train = y_train.astype('float32')
-y_train /= 255.  # scale masks to [0, 1]
-y_train = y_train.astype('uint8')
-
-# DB definition
-
-X_labeled_train = X_train[0:nb_labeled]
-y_labeled_train = y_train[0:nb_labeled]
-X_unlabeled_train = X_train[nb_labeled:len(X_train)]
-oracle = y_train[nb_labeled:len(X_train)]
-
-#Data augmentation
-
-datagen = ImageDataGenerator(
-    featurewise_center=True,
-    featurewise_std_normalization=True,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    horizontal_flip=True)
+labeled_index = np.arange(0,nb_labeled)
+unlabeled_index = np.arange(nb_labeled,len(X_train))
 
 # (1) Initialize model
-
 model = get_unet(dropout=True)
-weights = final_weights_path
-model_checkpoint = ModelCheckpoint(weights, monitor='loss', save_best_only=True)
-
+model.load_weights(initial_weights_path)
 if(initial_train):
+  model_checkpoint = ModelCheckpoint(initial_weights_path, monitor='loss', save_best_only=True)
 
-  if(augmentation):
-    model.fit_generator(datagen.flow(X_labeled_train, y_labeled_train, batch_size=32, shuffle=True),
-                    steps_per_epoch=len(X_labeled_train), nb_epoch=nb_initial_epochs, 
+  if (apply_augmentation):
+    for i in range(0,nb_initial_epochs):
+        history = model.fit_generator(data_generator().flow(X_train[labeled_index], y_train[labeled_index], 
+                    batch_size=32, shuffle=True),
+                    steps_per_epoch=len(labeled_index), nb_epoch=1, 
                     verbose=1, callbacks=[model_checkpoint])
+        
+        model.save(global_path+"initmodel"+str(i)+".h5")
+        log(history, i, log_file)
   else:
-    model.fit(X_labeled_train, y_labeled_train, batch_size=32, nb_epoch=nb_initial_epochs, verbose=1,
-              shuffle=True, callbacks=[model_checkpoint])
+    history = model.fit(X_train[labeled_index], y_train[labeled_index], batch_size=32, 
+              nb_epoch=nb_initial_epochs, verbose=1, shuffle=True, callbacks=[model_checkpoint])
+    log(history, i, log_file)
 
 else:
-  weights = initial_weights_path
   model.load_weights(initial_weights_path)
 
+
 # Active loop
+model_checkpoint = ModelCheckpoint(final_weights_path, monitor='loss', save_best_only=True)
 
-for iteration in range(0, nb_iterations):
+for iteration in range(1, nb_iterations+1):
 
-    if iteration > 0 and initial_train:
+    if iteration == 1:
+      weights = initial_weights_path
+
+    else:
       weights = final_weights_path
+      
 
     # (2) Labeling
-    
-    predictions_rank = uncertain_rank(X_unlabeled_train, nb_step_predictions, uncertain_method, weights)
-    
-    X_oracle_train = X_unlabeled_train[predictions_rank[0:nb_annotations]]
-    y_oracle_train = oracle[predictions_rank[0:nb_annotations]]
-
-    X_pseudo_train = X_unlabeled_train[predictions_rank[len(predictions_rank) - nb_pseudo:len(predictions_rank)]]
-    y_pseudo_train = predict(X_pseudo_train, weights)
-
-    np.save("x"+str(iteration),X_oracle_train)
-    np.save("y"+str(iteration),y_oracle_train)
-
-    X_labeled_train_aux = np.concatenate((X_labeled_train, X_oracle_train, X_pseudo_train))
-    y_labeled_train_aux = np.concatenate((y_labeled_train, y_oracle_train, y_pseudo_train))
-
-    save_logs(predictions_rank, X_unlabeled_train, iteration, weights)
+    X_labeled_train, y_labeled_train, labeled_index, unlabeled_index = compute_train_sets(X_train, y_train, 
+              labeled_index, unlabeled_index, weights, iteration)
 
     # (3) Training
+    history = model.fit(X_labeled_train, y_labeled_train, batch_size=32, nb_epoch=nb_active_epochs, verbose=1,
+              shuffle=True, callbacks=[model_checkpoint]) 
 
-    model.fit(X_labeled_train_aux, y_labeled_train_aux, batch_size=32, nb_epoch=nb_active_epochs, verbose=1,
-              shuffle=True, callbacks=[model_checkpoint])
+    log(history, iteration, log_file)
+    model.save(global_path+"models/model"+str(iteration)+".h5")
 
 
-    X_labeled_train = np.concatenate((X_labeled_train, X_oracle_train))
-    y_labeled_train = np.concatenate((y_labeled_train, y_oracle_train))
-    X_unlabeled_train = np.delete(X_unlabeled_train, predictions_rank[0:nb_annotations], 0)
-    oracle = np.delete(oracle, predictions_rank[0:nb_annotations], 0)
+log_file.close()
