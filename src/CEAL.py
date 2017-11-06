3b72b8e2163d7b9d97ff7b0f5b73f8a9502c5f75
Marc Górriz Blanch <gbmarc@hotmail.es>
2017-11-07 00:12:47 +0100

    Update CEAL.py

--- src/CEAL.py
+++ src/CEAL.py
@@ -16,11 +16,13 @@ unlabeled_index = np.arange(nb_labeled, len(X_train))
 # (1) Initialize model
 model = get_unet(dropout=True)
 model.load_weights(initial_weights_path)
+
 if initial_train:
     model_checkpoint = ModelCheckpoint(initial_weights_path, monitor='loss', save_best_only=True)
-
+    
     if apply_augmentation:
         for i in range(0, nb_initial_epochs):
+            
             history = model.fit_generator(
                 data_generator().flow(X_train[labeled_index], y_train[labeled_index], batch_size=32, shuffle=True),
                 steps_per_epoch=len(labeled_index), nb_epoch=1, verbose=1, callbacks=[model_checkpoint])
@@ -30,8 +32,8 @@ if initial_train:
     else:
         history = model.fit(X_train[labeled_index], y_train[labeled_index], batch_size=32, nb_epoch=nb_initial_epochs,
                             verbose=1, shuffle=True, callbacks=[model_checkpoint])
+        
         log(history, 0, log_file)
-
 else:
     model.load_weights(initial_weights_path)
 
@@ -39,10 +41,9 @@ else:
 model_checkpoint = ModelCheckpoint(final_weights_path, monitor='loss', save_best_only=True)
 
 for iteration in range(1, nb_iterations + 1):
-
     if iteration == 1:
         weights = initial_weights_path
-
+        
     else:
         weights = final_weights_path
 
@@ -51,11 +52,10 @@ for iteration in range(1, nb_iterations + 1):
                                                                                           labeled_index,
                                                                                           unlabeled_index, weights,
                                                                                           iteration)
-
     # (3) Training
     history = model.fit(X_labeled_train, y_labeled_train, batch_size=32, nb_epoch=nb_active_epochs, verbose=1,
                         shuffle=True, callbacks=[model_checkpoint])
-
+    
     log(history, iteration, log_file)
     model.save(global_path + "models/active_model" + str(iteration) + ".h5")
 
